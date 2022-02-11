fa5f8d83949e889de9690e301783e018d4984963
Alexandre BG <alexandre.brungiglio@nextalys.com>
2022-02-11 02:35:18 +0100

    minimal configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Environment } from './environment/environment';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: Environment.db_host,
+      port: 3306,
+      username: Environment.db_user,
+      password: Environment.db_password,
+      database: Environment.db_name,
+      logging: Environment.db_log_enabled,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      extra: { timezone: 'utc' },
+    }),
+    ConfigModule.forRoot({ envFilePath: './environment/.env', isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({});
   const config = new DocumentBuilder()
     .setTitle('ChillTV API')
     .setDescription('The chill TV API description')

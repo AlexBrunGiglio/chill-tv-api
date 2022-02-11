5ecea66bb2360bdcea2ca7f93b1192dd109c65e1
Alexandre BG <alexandre.brungiglio@nextalys.com>
2022-02-11 02:10:32 +0100

    add swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('ChillTV API')
+    .setDescription('The chill TV API description')
+    .setVersion('1.0')
+    .addTag('chilltv')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3020);
 }
 bootstrap();
